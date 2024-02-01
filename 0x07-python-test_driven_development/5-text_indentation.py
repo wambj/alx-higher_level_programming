51785820322c5c2eca6a5028c1ab5ddb891b3b90
Judy.Kibe <judykibe20@gmail.com>
2024-02-01 21:55:30 +0300

    Text Indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a text-indentation function"""
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("Test must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == '':
+        c += 1
+
+    while c < len(text):
+        print(text[c]. end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 
+            while c < len(text) and text[c] == '':
+                c += 1
+            continue
+        c += 1
