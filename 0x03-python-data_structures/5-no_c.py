5896bf640e4f57ad78c750f4599d6c165d5cbc73
Judy Kibe <judykibe20@gmail.com>
2024-02-17 07:45:53 -0800

    A string with no C's

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for c in my_string:
-        if c not in "Cc":
-            new_string += c
-        return new_string
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
