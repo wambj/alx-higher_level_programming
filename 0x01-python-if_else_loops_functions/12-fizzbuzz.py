bfc93f5c1d919a74d1c5464acb9191d65f6d9fc3
Judy Kibe <judykibe20@gmail.com>
2024-02-17 04:39:48 -0800

    Whitespace

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
-            print("Fizzbuzz ", end=' ')
+            print("Fizzbuzz", end=' ')
         elif number % 3 == 0:
             print("Fizz", end=' ')
         elif number % 5 == 0:
