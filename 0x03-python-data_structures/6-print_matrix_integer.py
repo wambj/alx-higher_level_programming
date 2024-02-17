d6301368f1da53977fa96ab9ae64bff51a11c0b9
Judy Kibe <judykibe20@gmail.com>
2024-02-17 08:00:04 -0800

    A string with no C's

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for col in range(len(row)):
-            print("{:d}".format(row[col]),
-                    end=' ' if col < len(row) - 1 else '')
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
             print()
