e361516c5425e0ef332294d2bad6192cfd277e95
Wambj <judykibe20@gmail.com>
2024-04-01 03:19:25 -0700

    States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- create database and table
+-- cat 6-states.sql | mysql -hlocalhost -uroot -p
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE, name VARCHAR(256));
