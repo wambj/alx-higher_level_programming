f241657c6921a0f25a62df9fa9b74a105bb50408
Wambj <judykibe20@gmail.com>
2024-04-01 03:12:09 -0700

    Unique ID

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+-- create table with a default value
+-- cat 5-unique_id.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
