31ff8758a751d4afb818583a3b38b571223b0aef
Wambj <judykibe20@gmail.com>
2024-04-03 00:23:07 -0700

    Number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- USE LEFT JOIN AND COUNT
+--  cat 13-count_shows_by_genre.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+SELECT b.name AS genre, COUNT(a.show_id) AS number_of_shows FROM tv_show_genres a LEFT JOIN tv_genres b ON a.genre_id = b.id GROUP BY a.genre_id ORDER BY 2 DESC
